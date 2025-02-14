f0bc7addbf8ead4a5608135c2c9a1b2e44262b1d
ismail ziyaadh <ismailziyaadh957@gmail.com>
2025-02-14 06:25:44 +0500

    added ssl and sslvalidate

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  ssl: true,
+  sslValidate: true
 });
 
 const db = mongoose.connection;
